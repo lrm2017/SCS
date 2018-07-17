dd5f970ebbfa2f5ff1ea7106d6fb7f435aecfafd
lrm_2017 <m13522639355@163.com>
2018-07-17 10:19:40 +0800

    添加了信息界面，更改侧滑栏，加了Student数据库类，添加了背景

--- app/src/main/java/com/example/scs/MyCourses.java
+++ app/src/main/java/com/example/scs/MyCourses.java
@@ -3,20 +3,38 @@ package com.example.scs;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by 123 on 2018/7/16.
  */
 
 public class MyCourses extends Fragment {
+    private View view;
+    Button button;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.test1,container,false);
+        if (view == null)
+            view = inflater.inflate(R.layout.test1,container,false);
+        ViewGroup parent = (ViewGroup) view.getParent();
+        if (parent != null)
+            parent.removeView(view);
+        button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"成功了",Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 
     @Override
@@ -24,5 +42,32 @@ public class MyCourses extends Fragment {
         super.onActivityCreated(savedInstanceState);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (getUserVisibleHint()){
+            showdata();
+        }
+    }
 
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        // 每次切换fragment时调用的方法
+        if (isVisibleToUser) {
+            showdata();
+        }
+    }
+
+    public static boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if (keyCode == event.KEYCODE_BACK) {
+            Log.d("GameFragmet事件", "OK");
+        }
+        return true;
+    }
+
+    private void showdata() {
+        //Toast.makeText(getActivity(), "Game", Toast.LENGTH_SHORT).show();
+    }
 }
